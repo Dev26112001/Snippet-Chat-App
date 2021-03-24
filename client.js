c398f1c3c09c2287d73b6572c9ea97c114c7f9d6
Devjyoti Mohanty <68328634+Dev26112001@users.noreply.github.com>
2021-03-24 19:07:23 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,42 @@
+const e = require("cors");
+
+const socket = io(http://localhost:8000)
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".container")
+var audio = new Audio('notification_sound.mp3.mp3');
+
+const append = (message, position) =>{
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add('position');
+    messageContainer.append(messageElement);
+
+}
+
+form.addEventListener('submit', () =>{
+    e.preventDefault();
+    const message = messageInput.valur;
+    append('You : ${message}', 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
+}
+const name = prompt("Enter your name to join ")
+socket.emit('new-user-joined', name)
+
+socket.on('user-joined', name =>{
+    append('${name} joined the chat', 'right')
+
+})
+
+socket.on('receive', data =>{
+    append('${data.user}: ${data.user}', 'left')
+
+})
+
+socket.on('leave', name =>{
+    append('${name} left the chat', 'left')
+
+})
